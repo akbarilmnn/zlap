30a6e309c95bfd8a961a57c83d64ca30d1acccff
akbar <akbarsmp27@gmail.com>
2024-09-30 23:06:48 +0700

    Changes: 1. refactor flag parsing code 2. TODO: handle flags with multiple arguments.

--- src/iter.zig
+++ src/iter.zig
@@ -3,14 +3,17 @@ const Allocator = std.mem.Allocator;
 // Iterate over CLI to find flags (options).
 pub const FlagIterator = struct {
     const Self = @This();
+    // Holds user-input CLI arguments.
     args: []const []const u8,
+    // Used to allocate CLI arguments when initialized using `init`.
     allocator: ?Allocator = null,
+    // A counter specifically to handle combined options.
     counter: usize = 0,
 
-    // struct representation of an option that may have a value.
+    // Struct representation of an option that may have a value.
     const Flag = struct {
         flag: []const u8,
-        value: ?[]const u8 = null,
+        argument: ?[]const u8 = null,
 
         fn flag(name: []const u8) Flag {
             return Flag{
@@ -21,33 +24,37 @@ pub const FlagIterator = struct {
         fn pairs(key: []const u8, value: []const u8) Flag {
             return Flag{
                 .flag = key,
-                .value = value,
+                .argument = value,
             };
         }
     };
 
+    // Initialize heap-allocated CLI arguments from the user.
     pub fn init(allocator: Allocator) Self {
         const args: []const []const u8 = std.process.argsAlloc(allocator) catch |err|
-            catcher("ERROR: out of memory!", err);
+            err_catch("ERROR: out of memory!", err);
         return Self{
             .args = args,
             .allocator = allocator,
         };
     }
 
+    // Free heap-allocated CLI arguments (does nothing if invoked other than `init`).
     pub fn deinit(self: *Self) void {
         if (self.allocator) |allocator| {
             std.process.argsFree(allocator, @ptrCast(self.args));
         }
     }
 
-    pub fn init_with_args(args: []const []const u8) Self {
+    // Initialize hypothetical CLI arguments (useful for testing).
+    pub fn initWithArgs(args: []const []const u8) Self {
         return Self{
             .args = @ptrCast(args),
             .allocator = null,
         };
     }
 
+    // For zig's iterator interface.
     pub fn next(self: *Self) ?Flag {
         for (self.args, 0..) |arg, idx| {
             if (isLongOption(arg)) {
@@ -65,58 +72,25 @@ pub const FlagIterator = struct {
     /////////////////////////////////////////////////
 
     fn handleLongOption(self: *Self, idx: usize) Flag {
+        // Get detected flag / option by accessing self.args using an index (idx).
         const optname = self.args[idx];
-        // strip `--` prefix
+        // Strip `--` prefix
         const stripped = optname[2..];
-        if (idx == self.args.len - 1) {
-            self.args = self.args[idx + 1 ..];
-            return Flag.flag(stripped);
-        }
-
-        const value = self.args[idx + 1];
-
-        if (!isOption(value)) {
-            self.args = self.args[idx + 1 ..];
-            return Flag.pairs(stripped, value);
-        }
-
-        self.args = self.args[idx + 1 ..];
-        return Flag.flag(stripped);
+        return self.getFlagGotoNext(idx, stripped);
     }
 
     fn handleShortOption(self: *Self, idx: usize) Flag {
-        // can either be single (e.g: -t) or combined (e.g: -xvf or -xvf 10).
+        // Can either be single (e.g: -t) or combined (e.g: -xvf or -xvf 10).
         const optname = self.args[idx];
-        // strip `-` prefix
+        // Strip `-` prefix
         const stripped = optname[1..];
 
-        // for the combined case we want to transform it into (e.g: -x -v -f or -x -v -f 10).
-        if (stripped.len == 0) {
-            self.args = self.args[idx + 1 ..];
-            self.counter = 0;
-            return Flag.flag("-");
-        }
-
+        // Single flag case.
         if (stripped.len == 1) {
-            if (idx == self.args.len - 1) {
-                self.args = self.args[idx + 1 ..];
-                self.counter = 0;
-                return Flag.flag(stripped);
-            }
-
-            const value = self.args[idx + 1];
-
-            if (!isOption(value)) {
-                self.args = self.args[idx + 1 ..];
-                self.counter = 0;
-                return Flag.pairs(stripped, value);
-            }
-
-            self.args = self.args[idx + 1 ..];
-            self.counter = 0;
-            return Flag.flag(stripped);
+            return self.getFlagGotoNext(idx, stripped);
         }
 
+        // for the combined case we want to interpret it as: -x -v -f or -x -v -f 10
         for (stripped) |_| {
             // start and middle part of combined flag.
             const flag = stripped[self.counter .. self.counter + 1];
@@ -125,39 +99,52 @@ pub const FlagIterator = struct {
                 return Flag.flag(flag);
             }
             // end part of combined flag.
-            if (idx == self.args.len - 1) {
-                self.args = self.args[idx + 1 ..];
-                self.counter = 0;
-                return Flag.flag(flag);
-            }
-
-            const value = self.args[idx + 1];
-
-            if (!isOption(value)) {
-                self.args = self.args[idx + 1 ..];
-                self.counter = 0;
-                return Flag.pairs(flag, value);
-            }
-
-            self.args = self.args[idx + 1 ..];
-            self.counter = 0;
-            return Flag.flag(flag);
+            return self.getFlagGotoNext(idx, flag);
         }
         unreachable;
     }
 
-    fn catcher(comptime msg: []const u8, err: anyerror) noreturn {
+    // A helper function to minimalize duplication for `handleLongOption` and `handleShortOption`.
+    fn getFlagGotoNext(self: *Self, idx: usize, stripped_flag: []const u8) Flag {
+        // Reset counter used for handling combined options.
+        self.counter = 0;
+        // Case 1: when it is an option that doesn't have any arguments but it is in the end of `self.args`.
+        if (idx == self.args.len - 1) {
+            self.args = self.args[idx + 1 ..];
+            return Flag.flag(stripped_flag);
+        }
+
+        // Case 2: when it is an option that has arguments and it isn't in the end of `self.args`.
+        // It is now safe to access the value i.e arguments of the flag because of a guard placed above.
+        const value = self.args[idx + 1];
+        if (!isOption(value)) {
+            self.args = self.args[idx + 1 ..];
+            return Flag.pairs(stripped_flag, value);
+        }
+
+        // Case 3: when the argument position of a flag is another flag.
+        self.args = self.args[idx + 1 ..];
+        return Flag.flag(stripped_flag);
+    }
+
+    // Helper fucntion to catch unrecoverable errors (panics).
+    fn err_catch(comptime msg: []const u8, err: anyerror) noreturn {
         std.debug.panic(msg ++ "{s}", .{@errorName(err)});
     }
 
+    // Helper function to check how valid an option's argument.
     fn isOption(str: []const u8) bool {
         return isLongOption(str) or isShortOption(str);
     }
 
+    // Check for long flags/options. It is considered to be when
+    // it has `--` prefix and it has at least 2 characters after the prefix.
     fn isLongOption(str: []const u8) bool {
         return std.mem.startsWith(u8, str, "--") and str.len >= 4;
     }
 
+    // Check for short flags/options. It is considered to be when
+    // it has `-` prefix and it has at least 1 characters after the prefix.
     fn isShortOption(str: []const u8) bool {
         return std.mem.startsWith(u8, str, "-") and str.len >= 2;
     }
--- src/unit_tests.zig
+++ src/unit_tests.zig
@@ -4,7 +4,7 @@ const eql = std.mem.eql;
 const Iter = @import("iter.zig").FlagIterator;
 
 test "initialize Iter using init_with_args" {
-    const it = Iter.init_with_args(&.{ "hello", "world" });
+    const it = Iter.initWithArgs(&.{ "hello", "world" });
     _ = it;
     try expect(true);
 }
@@ -17,27 +17,29 @@ test "initialize Iter using init" {
 }
 
 test "parse long options" {
-    var iter = Iter.init_with_args(&.{ "program", "--boot", "grub", "--start", "10" });
+    var iter = Iter.initWithArgs(&.{ "program", "--boot", "grub", "--start", "10", "-" });
     const pair = iter.next().?;
     try expect(eql(u8, pair.flag, "boot"));
-    try expect(eql(u8, pair.value.?, "grub"));
+    try expect(eql(u8, pair.argument.?, "grub"));
     const another_pair = iter.next().?;
     try expect(eql(u8, another_pair.flag, "start"));
-    try expect(eql(u8, another_pair.value.?, "10"));
+    try expect(eql(u8, another_pair.argument.?, "10"));
+    // doens't catch the last one!
+    try expect(iter.next() == null);
 }
 
 test "parse trailing long options" {
-    var iter = Iter.init_with_args(&.{ "program", "--foo", "10", "--exec" });
+    var iter = Iter.initWithArgs(&.{ "program", "--foo", "10", "--exec" });
     const pair = iter.next().?;
     try expect(eql(u8, pair.flag, "foo"));
-    try expect(eql(u8, pair.value.?, "10"));
+    try expect(eql(u8, pair.argument.?, "10"));
     const another_pair = iter.next().?;
     try expect(eql(u8, another_pair.flag, "exec"));
-    try expect(another_pair.value == null);
+    try expect(another_pair.argument == null);
 }
 
 test "parse short options" {
-    var iter = Iter.init_with_args(&.{ "program", "-l", "-t", "-a" });
+    var iter = Iter.initWithArgs(&.{ "program", "-l", "-t", "-a" });
     const pair = iter.next().?;
     try expect(eql(u8, pair.flag, "l"));
     const another_pair = iter.next().?;
@@ -47,7 +49,7 @@ test "parse short options" {
 }
 
 test "parse combined trailing short options" {
-    var iter = Iter.init_with_args(&.{ "program", "-lta" });
+    var iter = Iter.initWithArgs(&.{ "program", "-lta" });
     const pair = iter.next().?;
     try expect(eql(u8, pair.flag, "l"));
     const another_pair = iter.next().?;
@@ -57,14 +59,14 @@ test "parse combined trailing short options" {
 }
 
 test "parse trailing short options with value at the end" {
-    var iter = Iter.init_with_args(&.{ "program", "-lta", "10", "-bcd", "20" });
+    var iter = Iter.initWithArgs(&.{ "program", "-lta", "10", "-bcd", "20" });
     const l = iter.next().?;
     try expect(eql(u8, l.flag, "l"));
     const t = iter.next().?;
     try expect(eql(u8, t.flag, "t"));
     const a = iter.next().?;
     try expect(eql(u8, a.flag, "a"));
-    try expect(eql(u8, a.value.?, "10"));
+    try expect(eql(u8, a.argument.?, "10"));
 
     const b = iter.next().?;
     try expect(eql(u8, b.flag, "b"));
@@ -72,12 +74,5 @@ test "parse trailing short options with value at the end" {
     try expect(eql(u8, c.flag, "c"));
     const d = iter.next().?;
     try expect(eql(u8, d.flag, "d"));
-    try expect(eql(u8, d.value.?, "20"));
-}
-
-test "print flags" {
-    var it = Iter.init_with_args(&.{ "--foo", "bar", "--bah", "baz", "--bang", "--dung", "-a", "-c", "-lama", "yoo" });
-    while (it.next()) |a| {
-        std.debug.print("key: {s} and value: {?s}\n", .{ a.flag, a.value });
-    }
+    try expect(eql(u8, d.argument.?, "20"));
 }
